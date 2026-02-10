80ac83f5ebc23b94e7f03e7feae5a942f927c0b6
Olivier RATIEVILLE <olivier.ratieville@gmail.com>
2026-02-10 08:45:57 +0100

    Add profil view with links to edit / remove

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 final class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'app_profil')]
-    public function index(): Response
+    public function index(UserInterface $user): Response
     {
         return $this->render('profil/index.html.twig', [
-            'controller_name' => 'ProfilController',
+            'user' => $user
+        ]);
+    }
+
+    #[Route('/profil/edit', name: 'edit_profil')]
+    public function edit(UserInterface $user, EntityManagerInterface $em) {
+        return $this->render('profil/edit.html.twig', [
+            'user' => $user
+        ]);
+    }
+
+    #[Route('/profil/delete', name: "delete_profil")]
+    public function delete(UserInterface $user, EntityManagerInterface $em) {
+        return $this->render('profil/delete.html.twig', [
+            'user' => $user
         ]);
     }
 }
